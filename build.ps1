01bc74b84e2b97eb32d1e350d13a876f9561bcad
Neboer <43609792+Neboer@users.noreply.github.com>
2022-09-23 19:54:32 +0800

    增加了自动构建的脚本，修改了package.json里的表述内容。

---
+++ build.ps1
@@ -0,0 +1,14 @@
+$src_root = $PSScriptRoot
+$build_folder = $args[0]
+if ("" -ne $build_folder )
+{
+    Set-Location $src_root
+    yarn build
+    Set-Location $src_root/docs
+    env NODE_OPTIONS=--openssl-legacy-provider yarn build
+    Move-Item -Path $src_root/dist -Destination $build_folder
+    Move-Item -Path $src_root/docs/src/.vuepress/dist -Destination $build_folder/doc
+    Copy-Item $src_root/src/assets/gallery -Destination $build_folder/img -Exclude gallery.json -Recurse
+} else {
+    Write-Host "useage:./build.ps1 output_folder"
+}
